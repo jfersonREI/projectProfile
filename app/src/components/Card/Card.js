6fbc2bb0c6fa1103c23e0dd712de14bae94a99d2
jfersonREI <96252197+jfersonREI@users.noreply.github.com>
2023-05-11 16:58:51 -0400

    test

--- app/src/components/Card/Card.js
+++ app/src/components/Card/Card.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Card = () => (
-  <div className="group block p-6 bg-rei-primary-blue-30 shadow-lg hover:bg-rei-primary-red-30">
+  <div className="group flex flex-col border border-rei-primary-red-90 p-6 bg-rei-primary-blue-30 shadow-lg hover:bg-rei-primary-red-30">
     <h1 className="font-sans text-md font-medium text-yellow group-hover:text-white">
       title
     </h1>
